0c99887871d1c08a4ee2026af6f79d3749b00e60
Aditya <adityabagari77@gmail.com>
2024-06-07 13:40:39 +0530

    07-06-2024 01:40 PM

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, session
+from flask import Flask, render_template, session, request, redirect, flash
 from flask_sqlalchemy import SQLAlchemy
 import bcrypt
 
@@ -6,7 +6,6 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 db = SQLAlchemy(app)
 app.secret_key = "secret_key"
-
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -23,16 +22,16 @@ class User(db.Model):
 
     def check_password(self, password):
         return bcrypt.checkpw(password.encode("utf-8"), self.password.encode("utf-8"))
-
-
+    
 with app.app_context():
     db.create_all()
 
-
 @app.route("/")
 def index():
-    return render_template("index.html")
-
+    if session.get("email"):
+        return redirect("/dashboard")
+    else:
+        return render_template("index.html")
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
@@ -42,10 +41,16 @@ def register():
         email = request.form["email"]
         password = request.form["password"]
 
+        existing_user = User.query.filter_by(email=email).first()
+        if existing_user:
+            flash("Email already exists. Please use a different email or login.", "error")
+            return redirect("/register")
+
+
         new_user = User(name=name, email=email, password=password)
         db.session.add(new_user)
         db.session.commit()
-        return redirect("/login")
+        return redirect("/dashboard")
 
     return render_template("register.html")
 
@@ -62,6 +67,7 @@ def login():
             session["email"] = user.email
             return redirect("/dashboard")
         else:
+            flash("User does not exist !")
             return render_template("login.html", error="Invalid user")
 
     return render_template("login.html")
@@ -69,17 +75,28 @@ def login():
 
 @app.route("/dashboard")
 def dashboard():
-    if session["email"]:
+    if session.get("email"):
         user = User.query.filter_by(email=session["email"]).first()
         return render_template("dashboard.html", user=user)
-
-    return redirect("/login")
+    else:
+        return redirect("/login")
 
 
 @app.route("/logout")
 def logout():
     session.pop("email", None)
     return redirect("/login")
+
+@app.route("/delete_account")
+def delete_account():
+    if session.get("email"):
+        user = User.query.filter_by(email=session["email"]).first()
+        if user:
+            db.session.delete(user)
+            db.session.commit()
+            session.pop("email", None)
+            return redirect("/")
+    return redirect("/")
 
 
 if __name__ == "__main__":
